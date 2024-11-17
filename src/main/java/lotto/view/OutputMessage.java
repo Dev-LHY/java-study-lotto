0d235e57db61b593ec1695b0debe8de4240b698f
Dev-LHY <dev.leehy@gmail.com>
2024-11-17 19:39:44 +0900

    fix(outputMessage): 출력 메세지 수정

--- src/main/java/lotto/view/OutputMessage.java
+++ src/main/java/lotto/view/OutputMessage.java
@@ -4,7 +4,7 @@ public enum OutputMessage {
     INPUT_MONEY("구입금액을 입력해 주세요."),
     PURCHASE_AMOUNT("%d개를 구매했습니다."),
     INPUT_WINNING_NUMBERS("당첨 번호를 입력해 주세요."),
-    INPUT_BONUS_NUMBER("당첨 번호를 입력해 주세요."),
+    INPUT_BONUS_NUMBER("보너스 번호를 입력해 주세요."),
     WINNING_STATISTICS(
             "당첨 통계" + System.lineSeparator() +
                     "---" + System.lineSeparator() +
