980b428e3b70b8cba1913c28a5cf04eede434955
Dev-LHY <dev.leehy@gmail.com>
2024-11-17 21:33:39 +0900

    feat(lottoService): 로또 번호 적중 개수 getter 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,6 +1,7 @@
 package lotto.service;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import lotto.model.Rank;
 
@@ -11,5 +12,11 @@ public class LottoService {
         for (Rank rank : Rank.values()) {
             winningStatistics.put(rank, 0);
         }
+    }
+
+    private int getMatchCount(List<Integer> numbers, List<Integer> winningNumber) {
+        return (int) numbers.stream()
+                .filter(winningNumber::contains)
+                .count();
     }
 }
