2615b92358c485cfc685c7e0b323845011c0e88f
Dev-LHY <dev.leehy@gmail.com>
2024-11-17 22:56:57 +0900

    feat(exceptionMessage): 예외 메세지 추가

--- src/main/java/lotto/exception/ExceptionMessage.java
+++ src/main/java/lotto/exception/ExceptionMessage.java
@@ -3,6 +3,7 @@ package lotto.exception;
 public enum ExceptionMessage {
     EXCEPTION_PREFIX("[ERROR] "),
     SIZE_ERROR("로또 번호는 6개여야 합니다."),
+    DUPLICATE_LOTTO_NUMBER("로또 번호는 중복될 수 없습니다."),
     INVALID_INPUT("1~45 사이의 숫자만 입력 가능합니다."),
     INVALID_MONEY_UNIT("구입 금액은 1,000원 단위로만 가능합니다."),
     DUPLICATE_WINNING_NUMBER("당첨 번호는 중복될 수 없습니다."),
