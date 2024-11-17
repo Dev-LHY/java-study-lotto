bd25cbb9d72044ed83efc71928a28e5b160040a6
Dev-LHY <dev.leehy@gmail.com>
2024-11-17 21:38:58 +0900

    feat(lottoService): 당첨 통계 갱신 추가

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -27,6 +27,15 @@ public class LottoService {
         return (totalPrize / money) * SCALING_FACTOR;
     }
 
+    private void updateWinningStatistics(int matchCount, boolean isBonusMatch) {
+        for (Rank rank : Rank.values()) {
+            if (rank.checkRank(matchCount, isBonusMatch)) {
+                Integer count = winningStatistics.get(rank);
+                winningStatistics.put(rank, ++count);
+            }
+        }
+    }
+
     private void initializeWinningStatistics() {
         for (Rank rank : Rank.values()) {
             winningStatistics.put(rank, 0);
